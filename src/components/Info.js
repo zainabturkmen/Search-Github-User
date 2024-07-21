cdc303ecb714ecda295c27cf3b1d1da20e93bc33
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-21 17:49:58 +0430

    updating

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { GoRepo, GoGist } from "react-icons/go";
+import { FiUsers, FiUserPlus } from "react-icons/fi";
 
 const UserInfo = () => {
   const { githubUser } = React.useContext(GithubContext);
@@ -11,37 +11,37 @@ const UserInfo = () => {
   const items = [
     {
       id: 1,
-      icon: <GoRepo className='icon' />,
-      label: 'repos',
+      icon: <GoRepo className="icon" />,
+      label: "repos",
       value: public_repos,
-      color: 'pink',
+      color: "pink",
     },
     {
       id: 2,
-      icon: <FiUsers className='icon' />,
-      label: 'followers',
+      icon: <FiUsers className="icon" />,
+      label: "followers",
       value: followers,
-      color: 'green',
+      color: "green",
     },
     {
       id: 3,
-      icon: <FiUserPlus className='icon' />,
-      label: 'following',
+      icon: <FiUserPlus className="icon" />,
+      label: "following",
       value: following,
-      color: 'purple',
+      color: "purple",
     },
     {
       id: 4,
-      icon: <GoGist className='icon' />,
-      label: 'gists',
+      icon: <GoGist className="icon" />,
+      label: "gists",
       value: public_gists,
-      color: 'yellow',
+      color: "yellow",
     },
   ];
 
   return (
-    <section className='section'>
-      <Wrapper className='section-center'>
+    <section className="section">
+      <Wrapper className="section-center">
         {items.map((item) => {
           return <Item key={item.id} {...item}></Item>;
         })}
@@ -52,7 +52,7 @@ const UserInfo = () => {
 
 const Item = ({ icon, label, value, color }) => {
   return (
-    <article className='item'>
+    <article className="item">
       <span className={color}>{icon}</span>
       <div>
         <h3>{value}</h3>
